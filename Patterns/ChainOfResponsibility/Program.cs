682edbdbe6147ac205d449dd73fdbfa1e8c03fb6
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2023-08-01 10:02:01 +0300

    Refactoring Patterns

--- Patterns/ChainOfResponsibility/Program.cs
+++ Patterns/ChainOfResponsibility/Program.cs
@@ -7,42 +7,159 @@
  */
 class Program
 {
+    public static void GiveCommand(IWorker worker, string command)
+    {
+        var str = worker.Execute(command);
+        if(string.IsNullOrEmpty(str)) Console.WriteLine(command + " - никто не выполнил команду");
+        else Console.WriteLine(str);
+    }
+
     static void Main()
     {
+        #region Пример №1 - базовое
         Handler h1 = new ConcreateHandler1();
         Handler h2 = new ConcreateHandler2();
         h1.Successor = h2;
         h1.HandleRequest(2); //От первого до второго объекта обработка
+        Console.WriteLine("Please press Enter...");
         Console.ReadKey();
+        #endregion
+        #region Пример №2 - этапы строительства дома
+        var designer = new Designer();
+        var programmer = new Programmer();
+        var finishworker = new FinishWorker();
+        designer.SetNetWorker(finishworker).SetNetWorker(programmer);
+
+        GiveCommand(designer, "Спроектировать веранду");
+        GiveCommand(designer, "Сделать машину времени");
+        GiveCommand(designer, "Уволить работников");
+        GiveCommand(designer, "Курить");
+        #endregion
     }
 }
 
+/// <summary>
+/// Передатчик
+/// </summary>
 abstract class Handler
 {
     public Handler Successor { get; set; }
     public abstract void HandleRequest(int condition);
 }
 
+/// <summary>
+/// Обработчик запроса №1
+/// </summary>
 class ConcreateHandler1 : Handler
 {
+    /// <summary>
+    /// Обработка запроса
+    /// </summary>
+    /// <param name="condition">состояние</param>
     public override void HandleRequest(int condition)
-    {   //какая-то обработка запроса
+    {   
         Console.WriteLine("1");
-        if(condition == 1)
-            return;//завершаем выполнение
-        else if(Successor != null) //передача запроса дальше по цепи
+        if(condition == 1) return;              //завершаем выполнение
+        else if(Successor != null)              
+            Successor.HandleRequest(condition); //передача запроса дальше по цепи
+    }
+}
+
+/// <summary>
+/// Обработчик запроса №2
+/// </summary>
+class ConcreateHandler2 : Handler
+{
+    /// <summary>
+    /// Обработка запроса
+    /// </summary>
+    /// <param name="condition">состояние</param>
+    public override void HandleRequest(int condition)
+    {   
+        Console.WriteLine("2");
+        if (condition == 2) return;             //завершаем выполнение
+        else if (Successor != null)             //передача запроса дальше по цепи
             Successor.HandleRequest(condition);
     }
 }
 
-class ConcreateHandler2 : Handler
+/// <summary>
+/// Поведение рабочего
+/// </summary>
+interface IWorker
 {
-    public override void HandleRequest(int condition)
-    {   //какая-то обработка запроса
-        Console.WriteLine("2");
-        if (condition == 2)
-            return;//завершаем выполнение
-        else if (Successor != null) //передача запроса дальше по цепи
-            Successor.HandleRequest(condition);
+    /// <summary>
+    /// Передача обязанностей следующему рабочему
+    /// </summary>
+    /// <param name="worker">следующий рабочий</param >
+    IWorker SetNetWorker(IWorker worker);
+
+    /// <summary>
+    /// Рабочий принимает команду на исполнение
+    /// </summary>
+    /// <param name="command">команда</param>
+    /// <returns>Резульат принятия</returns>
+    string Execute(string command);
+}
+
+/// <summary>
+/// Абстрактный рабочий, базовое описание структуры каждого
+/// </summary>
+abstract class AbsWorker : IWorker
+{
+    private IWorker nextWorker;
+    public AbsWorker() => nextWorker = null;
+
+    /// <summary>
+    /// Изменяемый процесс обработки команды в классах наследниках
+    /// У каждого рабочего свой процесс выполнени
+    /// </summary>
+    /// <param name="command">команда</param>
+    /// <returns>Результат</returns>
+    public virtual string Execute(string command)
+    {
+        if (nextWorker == null) return string.Empty;
+        return nextWorker.Execute(command);
+    }
+
+    /// <summary>
+    /// Передача обязанностей другому рабочему
+    /// </summary>
+    /// <param name="worker">Другой рабочий</param>
+    /// <returns>Другой рабочий</returns>
+    public IWorker SetNetWorker(IWorker worker)
+    { 
+        nextWorker = worker;
+        return worker; 
+    }
+}
+
+class Designer : AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if (command == "Спроектировать веранду")
+            return "Проектировщик выполнил команду: " + command;
+        else return base.Execute(command);                       //если не может выполнить передаёт следующему в цепочке
+    }
+}
+
+class Programmer: AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if(command == "Сделать машину времени")
+            return "Программист выполнил команду: " + command;
+        else return base.Execute(command);
+    }
+}
+
+class FinishWorker : AbsWorker
+{
+    public override string Execute(string command)
+    {
+        if (command == "Уволить работников")
+            return "Начальник выполнил команду: " + command;
+        else return base.Execute(command);
     }
 }
--- Patterns/Command/Program.cs
+++ Patterns/Command/Program.cs
@@ -9,19 +9,39 @@ class Program
 {
     static void Main()
     {
+        #region Пример №1 - базовое
         var initCommand = new Invoker(new ConcreteCommand(new Receiver()));
         initCommand.Run();
         initCommand.Cancel();
+        Console.WriteLine("Please press Enter...");
         Console.ReadKey();
+        #endregion
+        #region Пример №2 - пульт управления конвеерной установкой
+        var conveyor = new Conveyor();   // создаём конвеер
+        var multipult = new Multipult(); // создаём пульт управления конвеером
+        multipult.SetCommand(0, new ConveyorWorkCommand(conveyor));
+        multipult.SetCommand(1, new ConveyorAjustCommand(conveyor));
+
+        multipult.PressOn(0);
+        multipult.PressOn(1);
+        multipult.PressCansel();
+        multipult.PressCansel();
+        #endregion
     }
 }
 
+/// <summary>
+/// Описания общего поведения объекта
+/// </summary>
 abstract class Command
 {
     public abstract void Execute();
     public abstract void Undo();
 }
 
+/// <summary>
+/// Описание процесса создания команды
+/// </summary>
 class ConcreteCommand : Command
 {
     Receiver receiver;
@@ -31,41 +51,175 @@ class ConcreteCommand : Command
         this.receiver = receiver;
     }
 
+    /// <summary>
+    /// Инициализация команды
+    /// *вызывает его получателя
+    /// </summary>
     public override void Execute()
     {
         receiver.Operation();
     }
 
+    /// <summary>
+    /// Остановка команды
+    /// </summary>
     public override void Undo()
     {
         Console.WriteLine("Stop");
     }
 }
 
-class Receiver //получатель поманды
+/// <summary>
+/// Описание возможностей получателя команды
+/// </summary>
+class Receiver
 {
+    /// <summary>
+    /// Обработка получателем команды
+    /// </summary>
     public void Operation()
     {
         Console.WriteLine("Processing...");
     }
 }
 
-class Invoker //инициатор команды
+/// <summary>
+/// Описание инициатора команды
+/// </summary>
+class Invoker
 {
     Command command;
 
+    /// <summary>
+    /// Принимает в себя команду
+    /// </summary>
+    /// <param name="command">#</param>
     public Invoker(Command command)
     {
         this.command = command;
     }
     
+    /// <summary>
+    /// Запускает команду
+    /// </summary>
     public void Run()
     {
         command.Execute();
     }
 
+    /// <summary>
+    /// Отменяет выполнение команды
+    /// </summary>
     public void Cancel()
     {
         command.Undo();
+    }
+}
+
+/// <summary>
+/// Поведение команды
+/// </summary>
+interface ICommand
+{
+    void Positive();
+    void Negative();
+}
+
+/// <summary>
+/// Класс конвеера
+/// </summary>
+class Conveyor
+{
+    public void On() => Console.WriteLine("Включение конвеера");
+
+    public void Off() => Console.WriteLine("Выключение конвеера");
+
+    public void SpeedIncrease() => Console.WriteLine("Скорость конвеера увеличена");
+
+    public void SpeedDecrease() => Console.WriteLine("Скорость конвеера снижена");
+}
+
+/// <summary>
+/// Класс управления работой конвеера
+/// </summary>
+class ConveyorWorkCommand : ICommand
+{
+    public Conveyor conveer;
+
+    /// <summary>
+    /// Передача типа конвеера в конструторе
+    /// </summary>
+    /// <param name="conveer">тип</param>
+    public ConveyorWorkCommand(Conveyor conveer) => this.conveer = conveer;
+
+    public void Negative() => conveer.Off();
+
+    public void Positive() => conveer.On();
+}
+
+/// <summary>
+/// Класс регулировки конвеера
+/// </summary>
+class ConveyorAjustCommand : ICommand
+{
+    public Conveyor conveer;
+
+    /// <summary>
+    /// Передача типа конвеера в конструторе
+    /// </summary>
+    /// <param name="conveer">тип</param>
+    public ConveyorAjustCommand(Conveyor conveer) => this.conveer = conveer;
+
+    public void Negative() => conveer.SpeedDecrease();
+
+    public void Positive() => conveer.SpeedIncrease();
+}
+
+
+/// <summary>
+/// Пульт управления конвеером
+/// </summary>
+class Multipult
+{
+    /// <summary>
+    /// Все возможные команды
+    /// </summary>
+    private List<ICommand> commands;
+    
+    /// <summary>
+    /// История выполненных команд для возможной их отмены
+    /// </summary>
+    private Stack<ICommand> history;
+
+    public Multipult()
+    {
+        commands = new List<ICommand>() { null, null };
+        history = new Stack<ICommand>();
+    }
+
+    /// <summary>
+    /// Устанавлием список команд по индексу кнопки
+    /// </summary>
+    public void SetCommand(int btn, ICommand command) => commands[btn] = command;
+
+    /// <summary>
+    /// Вызывает команду из списка по указанному индексу
+    /// и запишет в историю команд выполненную команду
+    /// </summary>
+    /// <param name="btn">идекс кнопки</param>
+    public void PressOn(int btn)
+    {
+        commands[btn].Positive();
+        history.Push(commands[btn]);
+    }
+
+    /// <summary>
+    /// Извлекает команду из истории и отменяет её
+    /// </summary>
+    public void PressCansel() 
+    {
+        if(history.Count == 0) return;
+        var oldC = history.Pop();
+        oldC.Negative();
     }
 }
